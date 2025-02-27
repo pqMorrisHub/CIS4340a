4e15e16120a24fff62f4178f0d9bac8a3e62031e
pqMorrisHub <142948561+pqMorrisHub@users.noreply.github.com>
2025-02-26 21:20:06 -0500

    Update R03_NUM03_J.java

--- R03_NUM03_J.java
+++ R03_NUM03_J.java
@@ -1,10 +1,4 @@
 /*
-BigInteger x = new BigInteger("530500452766");
-String s = x.toString();  // Valid character data
-byte[] byteArray = s.getBytes();
-String ns = new String(byteArray); 
-x = new BigInteger(ns);
-
 public static long getInteger (DataInputStream is) throws IOException {
   return is.readInt() & 0xFFFFFFFFL ; // Mask with 32 one−bits
 }
