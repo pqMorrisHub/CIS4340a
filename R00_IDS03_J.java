d4fa9bb61112b08f747b749a3de28f99ad0645be
pqMorrisHub <142948561+pqMorrisHub@users.noreply.github.com>
2025-02-26 21:07:55 -0500

    Create R00_IDS03_J.java

---
+++ R00_IDS03_J.java
@@ -0,0 +1,13 @@
+/*
+public String sanitizeUser(String username) {
+  return Pattern.matches("[A-Za-z0-9_]+", username))
+      ? username : "unauthorized user";
+}
+
+
+if (loginSuccessful) {
+  logger.severe("User login succeeded for: " + sanitizeUser(username));
+} else {
+  logger.severe("User login failed for: " + sanitizeUser(username));
+}
+*/
