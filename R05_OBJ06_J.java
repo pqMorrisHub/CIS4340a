7b40a60f85a8850ea12a49588cb20268804f971e
pqMorrisHub <142948561+pqMorrisHub@users.noreply.github.com>
2025-03-02 15:55:50 -0500

    Update R05_OBJ06_J.java

--- R05_OBJ06_J.java
+++ R05_OBJ06_J.java
@@ -2,15 +2,17 @@ public final class MutableDemo {
   // java.net.HttpCookie is mutable
   public void useMutableInput(HttpCookie cookie) {
     if (cookie == null) {
-       throw new NullPointerException();
+      throw new NullPointerException();
     }
+ 
+    // Create copy
+    cookie = (HttpCookie)cookie.clone();
  
     // Check whether cookie has expired
     if (cookie.hasExpired()) {
       // Cookie is no longer valid; handle condition by throwing an exception
     }
  
-    // Cookie may have expired since time of check
     doLogic(cookie);
   }
 }
