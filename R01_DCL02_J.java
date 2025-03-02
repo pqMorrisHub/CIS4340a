ce631a86356c395bb961f6e4c997410080ac3a63
pqMorrisHub <142948561+pqMorrisHub@users.noreply.github.com>
2025-03-02 15:53:56 -0500

    Update R01_DCL02_J.java

--- R01_DCL02_J.java
+++ R01_DCL02_J.java
@@ -2,13 +2,14 @@ List<Integer> list = Arrays.asList(new Integer[] {13, 14, 15});
 boolean first = true;
  
 System.out.println("Processing list...");
-for (Integer i: list) {
+for (final Integer i: list) {
+  Integer item = i;
   if (first) {
     first = false;
-    i = new Integer(99);
+    item = new Integer(99);
   }
-  System.out.println(" New item: " + i);
-  // Process i
+  System.out.println(" New item: " + item);
+  // Process item
 }
  
 System.out.println("Modified list?");
