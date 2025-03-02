a35786cf60bcbd160447a53a1f4460fe1e132495
pqMorrisHub <142948561+pqMorrisHub@users.noreply.github.com>
2025-03-02 15:43:23 -0500

    Update R03_NUM03_J.java

--- R03_NUM03_J.java
+++ R03_NUM03_J.java
@@ -1,5 +1,3 @@
-/*
-public static long getInteger (DataInputStream is) throws IOException {
-  return is.readInt() & 0xFFFFFFFFL ; // Mask with 32 one−bits
+public static int getInteger(DataInputStream is) throws IOException {
+  return is.readInt(); 
 }
-*/
