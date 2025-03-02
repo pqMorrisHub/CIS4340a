e80c4c3d9617c5aa3d2fa745745f284a755057f2
pqMorrisHub <142948561+pqMorrisHub@users.noreply.github.com>
2025-03-02 15:42:54 -0500

    Update R02_XP00_J.java

--- R02_XP00_J.java
+++ R02_XP00_J.java
@@ -2,6 +2,16 @@ public void deleteFile(){
  
   File someFile = new File("someFileName.txt");
   // Do something with someFile
-  someFile.delete();
+  if (!someFile.delete()) {
+    // Handle failure to delete the file
+  }
  
+}
+
+public class Replace {
+  public static void main(String[] args) {
+    String original = "insecure";
+    original = original.replace('i', '9');
+    System.out.println(original);
+  }
 }
