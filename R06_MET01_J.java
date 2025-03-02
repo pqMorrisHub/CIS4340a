a7509ed7a4270b74c9ad1cb0c853de72ec43f43d
pqMorrisHub <142948561+pqMorrisHub@users.noreply.github.com>
2025-03-02 15:47:14 -0500

    Update R06_MET01_J.java

--- R06_MET01_J.java
+++ R06_MET01_J.java
@@ -1,13 +1,8 @@
-/*
 public static int getAbsAdd(int x, int y) {
-  if (x == Integer.MIN_VALUE || y == Integer.MIN_VALUE) {
-    throw new IllegalArgumentException();
-  }
+  assert x != Integer.MIN_VALUE;
+  assert y != Integer.MIN_VALUE;
   int absX = Math.abs(x);
   int absY = Math.abs(y);
-  if (absX > Integer.MAX_VALUE - absY) {
-    throw new IllegalArgumentException();
-  }
+  assert (absX <= Integer.MAX_VALUE - absY);
   return absX + absY;
 }
-*/
